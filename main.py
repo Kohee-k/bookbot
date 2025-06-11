5cf1ae8afe079d49b4ecf132b190905bf9d95242
Kohee-k <sebastianx96@hotmail.com>
2025-06-11 19:57:51 +0200

    main.py program done

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def get_book_text()
-    
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+   path = get_book_text("books/frankenstein.txt")
+   print(f"{path}")
+
+
+main()
