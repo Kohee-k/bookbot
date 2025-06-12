5bac9705fc7e56802a4220c67da8bbe8a03cd9ac
Kohee-k <sebastianx96@hotmail.com>
2025-06-12 14:21:40 +0200

    character count function done

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def main():
    text = get_book_text(book_path)
    num_words = get_number_of_words(text)
    character_count = book_char_count(text)
-   print(f"{num_words} words found in the document, {character_count}")
+   print(f"{num_words} words found in the document")
+   print(character_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-from stats import book_char_count
+from stats import get_number_of_words, book_char_count
 
 main()
