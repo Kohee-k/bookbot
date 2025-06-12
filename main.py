e743588a21b60fcdd548b94def1c79613aafb01b
Kohee-k <sebastianx96@hotmail.com>
2025-06-12 14:12:47 +0200

    working on character count function

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ def main():
    book_path = "books/frankenstein.txt"
    text = get_book_text(book_path)
    num_words = get_number_of_words(text)
-   print(f"{num_words} words found in the document")
+   character_count = book_char_count(text)
+   print(f"{num_words} words found in the document, {character_count}")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-from stats import get_number_of_words
+from stats import book_char_count
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def get_number_of_words(text):
     words = text.split()
     return len(words)
+
+def book_char_count(text):
+    char_count = {}
+    lowercase_version = text.lower()
+    characters = list(lowercase_version)
+    for char in characters:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    return char_count
+
+
+
+
