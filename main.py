2920de1fa4b75a6b5beba39ed888b87031454695
Kohee-k <sebastianx96@hotmail.com>
2025-06-12 12:30:36 +0200

    main.py again

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-"""
-def main():
-   path = get_book_text("books/frankenstein.txt")
-   print(f"{path}")
-"""
-def main():
+
+def get_number_of_words():
     num_words = 0
     text = get_book_text("books/frankenstein.txt")
     words = text.split()
     for word in words:
         num_words += 1
-    print(f"{num_words} words found in the document")
+    return num_words
+
+def main():
+   number_words = get_number_of_words()
+   print(f"{number_words} words found in the document")
+
+
 
 
 main()
