5c9159f885c1e9b9fe8f3435112be7c5684a07a8
Kohee-k <sebastianx96@hotmail.com>
2025-06-14 09:31:29 +0200

    cleaned main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_number_of_words, 
     book_char_count, 
@@ -11,6 +13,14 @@ def main():
    num_words = get_number_of_words(text)
    character_count = book_char_count(text)
    character_count_sorted = sorted_list(character_count)
+   print_report(book_path, num_words, character_count_sorted)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def print_report(book_path, num_words, character_count_sorted):
    print("============ BOOKBOT ============")
    print(f"Analyzing book found at {book_path}...")
    print("----------- Word Count ----------")
@@ -21,10 +31,6 @@ def main():
        char = char_dict["char"]
        print(f"{char}: {num}")
    print("============= END ===============")
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
 
 main()
